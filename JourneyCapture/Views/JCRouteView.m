a32c43b37d876616374997272a1a0018f87d76df
Chris Sloey <chris.sloey@gmail.com>
2014-05-13 15:38:39 +0100

    Route overview layout

--- JourneyCapture/Views/JCRouteView.m
+++ JourneyCapture/Views/JCRouteView.m
@@ -18,46 +18,34 @@
     if (!self) {
         return self;
     }
-    
+
     _viewModel = routeViewModel;
-    
-//    // Map
-//    _mapView = [MKMapView new];
-//    _mapView.delegate = self;
-//    _mapView.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_mapView];
-//    
-//    // Stats
-//    _timeTitleLabel = [UILabel new];
-//    _timeTitleLabel.text = @"Time";
-//    _timeTitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_timeTitleLabel];
-//    
-//    _timeLabel = [UILabel new];
-//    _timeLabel.text = @"3:15:30";
-//    _timeLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_timeLabel];
-//    
-//    _distanceTitleLabel = [UILabel new];
-//    _distanceTitleLabel.text = @"Distance";
-//    _distanceTitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_distanceTitleLabel];
-//    
-//    _distanceLabel = [UILabel new];
-//    _distanceLabel.text = @"34.5 miles";
-//    _distanceLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_distanceLabel];
-//    
-//    _avgSpeedTitleLabel = [UILabel new];
-//    _avgSpeedTitleLabel.text = @"Avg Speed";
-//    _avgSpeedTitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_avgSpeedTitleLabel];
-//    
-//    _avgSpeedLabel = [UILabel new];
-//    _avgSpeedLabel.text = @"26 mph";
-//    _avgSpeedLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//    [self addSubview:_avgSpeedLabel];
-    
+
+    // Map
+    _mapView = [MKMapView new];
+    _mapView.delegate = self;
+    _mapView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:_mapView];
+
+    // Stats
+    _timeLabel = [UILabel new];
+    _timeLabel.text = @"3:15:30";
+    _timeLabel.textAlignment = NSTextAlignmentCenter;
+    _timeLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:_timeLabel];
+
+    _distanceLabel = [UILabel new];
+    _distanceLabel.text = @"34.5 miles";
+    _distanceLabel.textAlignment = NSTextAlignmentCenter;
+    _distanceLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:_distanceLabel];
+
+    _avgSpeedLabel = [UILabel new];
+    _avgSpeedLabel.text = @"26 mph";
+    _avgSpeedLabel.textAlignment = NSTextAlignmentCenter;
+    _avgSpeedLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self addSubview:_avgSpeedLabel];
+
     // Star Rating
     _reviewStarView = [[EDStarRating alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
     _reviewStarView.editable = YES;
@@ -68,7 +56,7 @@
     _reviewStarView.rating = 3;
     _reviewStarView.backgroundColor = [UIColor whiteColor];
     [self addSubview:_reviewStarView];
-    
+
     return self;
 }
 
@@ -76,22 +64,22 @@
 {
     // Draw points
     NSUInteger numPoints = [self.viewModel.points count];
-    
+
     if (numPoints < 2) {
         return;
     }
-    
+
     MKMapPoint *pointsArray = malloc(sizeof(CLLocationCoordinate2D)*numPoints);
     for (int i = 0; i < numPoints; i++) {
         JCRoutePointViewModel *point = self.viewModel.points[i];
         pointsArray[i] = MKMapPointForCoordinate(point.location.coordinate);
     }
-    
+
     MKPolyline *routeLine = [MKPolyline polylineWithPoints:pointsArray count:numPoints];
     free(pointsArray);
-    
+
     [_mapView addOverlay:routeLine];
-    
+
     // Zoom to points
     MKMapRect zoomRect = MKMapRectNull;
     for (JCRoutePointViewModel *point in self.viewModel.points)
@@ -100,10 +88,10 @@
         MKMapRect pointRect = MKMapRectMake(annotationPoint.x, annotationPoint.y, 350.0, 350.0);
         zoomRect = MKMapRectUnion(zoomRect, pointRect);
     }
-    
+
     MKMapRect paddedZoomRect = MKMapRectMake(zoomRect.origin.x - 4000, zoomRect.origin.y -= 4000,
                                              zoomRect.size.width + 8000, zoomRect.size.height + 8000);
-    
+
     [_mapView setVisibleMapRect:paddedZoomRect animated:YES];
 }
 
@@ -111,16 +99,33 @@
 
 - (void)layoutSubviews
 {
+    // (bottom up)
+
+    // Review
     [_reviewStarView autoRemoveConstraintsAffectingView];
     [_reviewStarView autoPinEdge:ALEdgeBottom toEdge:ALEdgeBottom ofView:self withOffset:-10];
     [_reviewStarView autoSetDimension:ALDimensionHeight toSize:26];
     [_reviewStarView autoSetDimension:ALDimensionWidth toSize:150];
     [_reviewStarView autoAlignAxis:ALAxisVertical toSameAxisOfView:self];
-    
-//    [_mapView autoRemoveConstraintsAffectingView];
-//    [_mapView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeBottom];
-//    [_mapView autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:_timeTitleLabel];
-    
+
+    // Stats
+    [_timeLabel autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:_reviewStarView withOffset:-10];
+    [_timeLabel autoPinEdge:ALEdgeLeft toEdge:ALEdgeLeft ofView:self withOffset:10];
+    [_timeLabel autoSetDimension:ALDimensionWidth toSize:100];
+
+    [_distanceLabel autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:_reviewStarView withOffset:-10];
+    [_distanceLabel autoAlignAxis:ALAxisVertical toSameAxisOfView:self];
+    [_distanceLabel autoMatchDimension:ALDimensionWidth toDimension:ALDimensionWidth ofView:_timeLabel];
+
+    [_avgSpeedLabel autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:_reviewStarView withOffset:-10];
+    [_avgSpeedLabel autoPinEdge:ALEdgeRight toEdge:ALEdgeRight ofView:self withOffset:-10];
+    [_avgSpeedLabel autoMatchDimension:ALDimensionWidth toDimension:ALDimensionWidth ofView:_timeLabel];
+
+    // Map
+    [_mapView autoRemoveConstraintsAffectingView];
+    [_mapView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeBottom];
+    [_mapView autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:_timeLabel withOffset:-15];
+
     [super layoutSubviews];
 }
 
