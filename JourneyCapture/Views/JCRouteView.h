48da0803b04df21f478760b76c004ad0d745fef7
Chris Sloey <chris.sloey@gmail.com>
2014-05-13 14:57:44 +0100

    View elements created, star rating positioned and configured

--- JourneyCapture/Views/JCRouteView.h
+++ JourneyCapture/Views/JCRouteView.h
@@ -8,12 +8,23 @@
 
 @import UIKit;
 @import MapKit;
+#import "EDStarRating.h"
 @class JCRouteViewModel;
 
-@interface JCRouteView : UIView <MKMapViewDelegate>
+@interface JCRouteView : UIView <MKMapViewDelegate, EDStarRatingProtocol>
 
 @property (strong, nonatomic) JCRouteViewModel *viewModel;
 @property (strong, nonatomic) MKMapView *mapView;
+
+// Stats
+@property (strong, nonatomic) UILabel *timeTitleLabel;
+@property (strong, nonatomic) UILabel *timeLabel;
+@property (strong, nonatomic) UILabel *distanceTitleLabel;
+@property (strong, nonatomic) UILabel *distanceLabel;
+@property (strong, nonatomic) UILabel *avgSpeedTitleLabel;
+@property (strong, nonatomic) UILabel *avgSpeedLabel;
+
+@property (strong, nonatomic) EDStarRating *reviewStarView;
 
 - (id)initWithViewModel:(JCRouteViewModel *)routeViewModel;
 - (void)drawRoute;
--- JourneyCapture/Views/JCRouteView.m
+++ JourneyCapture/Views/JCRouteView.m
@@ -21,11 +21,53 @@
     
     _viewModel = routeViewModel;
     
-    // Map
-    _mapView = [MKMapView new];
-    _mapView.delegate = self;
-    _mapView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self addSubview:_mapView];
+//    // Map
+//    _mapView = [MKMapView new];
+//    _mapView.delegate = self;
+//    _mapView.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_mapView];
+//    
+//    // Stats
+//    _timeTitleLabel = [UILabel new];
+//    _timeTitleLabel.text = @"Time";
+//    _timeTitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_timeTitleLabel];
+//    
+//    _timeLabel = [UILabel new];
+//    _timeLabel.text = @"3:15:30";
+//    _timeLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_timeLabel];
+//    
+//    _distanceTitleLabel = [UILabel new];
+//    _distanceTitleLabel.text = @"Distance";
+//    _distanceTitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_distanceTitleLabel];
+//    
+//    _distanceLabel = [UILabel new];
+//    _distanceLabel.text = @"34.5 miles";
+//    _distanceLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_distanceLabel];
+//    
+//    _avgSpeedTitleLabel = [UILabel new];
+//    _avgSpeedTitleLabel.text = @"Avg Speed";
+//    _avgSpeedTitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_avgSpeedTitleLabel];
+//    
+//    _avgSpeedLabel = [UILabel new];
+//    _avgSpeedLabel.text = @"26 mph";
+//    _avgSpeedLabel.translatesAutoresizingMaskIntoConstraints = NO;
+//    [self addSubview:_avgSpeedLabel];
+    
+    // Star Rating
+    _reviewStarView = [[EDStarRating alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    _reviewStarView.editable = YES;
+    _reviewStarView.translatesAutoresizingMaskIntoConstraints = NO;
+    _reviewStarView.displayMode = EDStarRatingDisplayFull;
+    _reviewStarView.starImage = [UIImage imageNamed:@"star"];
+    _reviewStarView.starHighlightedImage = [UIImage imageNamed:@"filled-star"];
+    _reviewStarView.rating = 3;
+    _reviewStarView.backgroundColor = [UIColor whiteColor];
+    [self addSubview:_reviewStarView];
     
     return self;
 }
@@ -69,9 +111,15 @@
 
 - (void)layoutSubviews
 {
-    [_mapView autoRemoveConstraintsAffectingView];
-    [_mapView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeBottom];
-    [_mapView autoSetDimension:ALDimensionHeight toSize:362];
+    [_reviewStarView autoRemoveConstraintsAffectingView];
+    [_reviewStarView autoPinEdge:ALEdgeBottom toEdge:ALEdgeBottom ofView:self withOffset:-10];
+    [_reviewStarView autoSetDimension:ALDimensionHeight toSize:26];
+    [_reviewStarView autoSetDimension:ALDimensionWidth toSize:150];
+    [_reviewStarView autoAlignAxis:ALAxisVertical toSameAxisOfView:self];
+    
+//    [_mapView autoRemoveConstraintsAffectingView];
+//    [_mapView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeBottom];
+//    [_mapView autoPinEdge:ALEdgeBottom toEdge:ALEdgeTop ofView:_timeTitleLabel];
     
     [super layoutSubviews];
 }
