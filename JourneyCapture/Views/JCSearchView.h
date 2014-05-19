f674d8174e334ef18606791e96fc727386a6946e
mdhayes <mdhayes@gmx.com>
2014-05-19 15:55:54 +0100

    search view loads blue background

---
+++ JourneyCapture/Views/JCSearchView.h
@@ -0,0 +1,13 @@
+//
+//  JCSearchView.h
+//  JourneyCapture
+//
+//  Created by Michael Hayes on 19/05/2014.
+//  Copyright (c) 2014 FCD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCSearchView : UIView
+
+@end
---
+++ JourneyCapture/Views/JCSearchView.m
@@ -0,0 +1,31 @@
+//
+//  JCSearchView.m
+//  JourneyCapture
+//
+//  Created by Michael Hayes on 19/05/2014.
+//  Copyright (c) 2014 FCD. All rights reserved.
+//
+
+#import "JCSearchView.h"
+
+@implementation JCSearchView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
