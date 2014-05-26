972b789772c2f82a2b2c7ffab7566e2035e825b5
Chris Sloey <chris.sloey@gmail.com>
2014-05-26 16:26:29 +0100

    Cycle map view added to map VC

--- JourneyCapture/Views/JCCycleMapViewController.m
+++ JourneyCapture/Views/JCCycleMapViewController.m
@@ -29,10 +29,21 @@
 - (void)loadView
 {
     self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     _cycleMapView = [JCCycleMapView new];
     _cycleMapView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:_cycleMapView];
+}
+
+- (void)viewWillLayoutSubviews
+{
+    [super viewWillLayoutSubviews];
+    
+    [_cycleMapView autoPinToTopLayoutGuideOfViewController:self withInset:0];
+    [_cycleMapView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero excludingEdge:ALEdgeTop];
+    
+    [self.view layoutSubviews];
 }
 
 - (void)viewDidLoad
